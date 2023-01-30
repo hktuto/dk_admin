fd1de9bd04e4248046ca7c4a2734f70dd50fa3cb
Sean Tsang <seantsang83@gmail.com>
2023-01-30 13:47:36 +0800

    update

--- pages/User-Group/add.vue
+++ pages/User-Group/add.vue
@@ -3,10 +3,72 @@
         <div class="pageHeader">
             Create User Group
         </div>
+        <div class="form">
+            <ElForm>
+                <ElFormItem label="Name">
+                    <ElInput v-model="data.name" />
+                </ElFormItem > 
+                <ElFormItem label="user">
+                    <ElSelect v-model="data.user" multiple >
+                        <ElOption v-for="item in displayUserList" :key="item" :value="item.user_id" :label="item.username" />
+                    </ElSelect>
+
+                </ElFormItem>
+                <ElFormItem label="limit">
+                    <ElInput v-model="data.limit" />
+                </ElFormItem>
+                <ElFormItem>
+                    <ElButton type="primary" @click="submit">Submit</ElButton>
+                </ElFormItem>
+            </ElForm>
+        </div>
     </div>
 </template>
 
 
 <script lang="ts" setup>
+import { ElForm , ElFormItem, ElInput, ElButton, ElNotification, ElSelect, ElOption } from 'element-plus';
+const router = useRouter();
 
+const { data:userList, refresh } = await  useAsyncData('userList', () => apiFetch('/user_list', {method:'post'}))
+
+const displayUserList = computed(() => userList.value.filter(i => i.user_id && i.username))
+const data = useState('newAchievement', () => ({
+    name:'',
+    user_group_id:'',
+    user:[],
+    limit:10
+}))
+
+async function submit() {
+    if(!data.value.name || !data.value.limit ) {
+        ElNotification({
+            title: 'Warning',
+            message:'Please fill in all required field',
+            type: 'error'
+        })
+    }
+    if(Number(data.value.limit) === NaN) {
+        ElNotification({
+            title: 'Warning',
+            message:'Value must be number',
+            type: 'error'
+        })
+    }
+
+    await apiFetch('/userGroup/create', { method:'post', body:{
+        name: data.value.name,
+        user_group_id: data.value.name,
+        user: data.value.user,
+        limit:Number(data.value.limit)
+    }})
+    ElNotification({
+            title: 'Success',
+            message:'Record Created',
+            type: 'success'
+        })
+    router.push({
+        path:'/User-Group/list'
+    })
+}
 </script>
--- pages/User-Group/list.vue
+++ pages/User-Group/list.vue
@@ -6,18 +6,19 @@
         <div class="actionList">
             <ElButton @click="$router.push('/User-Group/add')">Create User Group</ElButton>
         </div>
-        <ElTable :data="data" @row-click="rowClick">
-            <ElTableColumn prop="email" label="email"/>
-            <ElTableColumn prop="username" label="username"/>
-            <ElTableColumn prop="mobileCountryCode" label="countryCode">
-               
+        <ElTable :data="displayUserGroup" @row-click="rowClick">
+            <ElTableColumn prop="name" label="name"/>
+            <ElTableColumn prop="parentId" label="parentId"/>
+            <ElTableColumn prop="user" label="user" >
+                <template #default="scope">
+                    <div class="userRow">
+                        <ElTag v-for="user in displayUserInGroup(scope.row.user)" :key="user.user_id">{{user.username}}</ElTag>
+                    </div>
+                </template>
             </ElTableColumn>
-            <ElTableColumn prop="mobile" label="Phone">
-            </ElTableColumn>
-
             <ElTableColumn>
                 <template #default="scope">
-                    <ElButton :icon="Delete" circle @click.stop="() => deleteItem(scope.row)" :disabled="scope.row.username ? scope.row.username.includes('system_') : true">
+                    <ElButton :icon="Delete" circle @click.stop="() => deleteItem(scope.row)" >
                     </ElButton>
                 </template>
             </ElTableColumn>
@@ -27,22 +28,32 @@
 </template>
 
 <script lang="ts" setup>
-import { ElTable, ElTableColumn, ElNotification, ElButton } from 'element-plus'
+import { ElTable, ElTableColumn, ElNotification, ElButton, ElTag } from 'element-plus'
 import { Delete } from '@element-plus/icons-vue'
-const { data, refresh } = await  useAsyncData('userGroupList', () => apiFetch('/user_group_read', {method:'post'}))
+const { data, refresh } = await  useAsyncData('userGroupList', () => apiFetch('/userGroup/all', {method:'get'}))
+
+const displayUserGroup = computed(() => data.value.filter( group => group.user_group_id ))
+const { data:userList } = await  useAsyncData('userList', () => apiFetch('/user_list', {method:'post'}))
+
+const displayUserList = computed(() => userList.value.filter(i => i.user_id && i.username))
+
+const displayUserInGroup = (ids:string[]) => {
+    return displayUserList.value.filter( u => ids.includes(u.user_id))
+}
+
 const router = useRouter()
 function rowClick(row, column, cell, event) {
     router.push({
-        path:'/User-Group/edit/' + row.user_id
+        path:'/User-Group/edit/' + row.user_group_id
     })
 }
 
 async function deleteItem(row) {
     
     const body = {
-        id: row._id.$oid
+        user_group_id: row.user_group_id
     }
-    await apiFetch('user_delete', { method:'post', body },  )
+    await apiFetch('/userGroup/delete', { method:'post', body },  )
     ElNotification({
         title:"Success",
         message:"Item Deleted",
@@ -52,3 +63,11 @@ async function deleteItem(row) {
 }
 onMounted(() => refresh())
 </script>
+
+<style scoped>
+.userRow{
+    display: flex;
+    flex-flow: row wrap;
+    gap: 12px;
+}
+</style>
